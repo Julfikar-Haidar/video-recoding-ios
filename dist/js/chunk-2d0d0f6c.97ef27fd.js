a9e0dd10377c370bc626e16589340deb425c526b
Julfikar Haidar <julfikar6262@gmail.com>
2021-02-18 17:08:10 +0600

    ios recoding build file up

---
+++ dist/js/chunk-2d0d0f6c.97ef27fd.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d0f6c"],{"69e8":function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var o=e("09c8"),c=(e("b948"),function(){var n=window;n.addEventListener("statusTap",(function(){Object(o["g"])((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var i=c.closest("ion-content");i&&i.componentOnReady().then((function(){Object(o["m"])((function(){return i.scrollToTop(300)}))}))}}))}))})}}]);
+//# sourceMappingURL=chunk-2d0d0f6c.97ef27fd.js.map
